77b05ea3f4603afa2bb0b4f689d9d53f1eb46511
ptone545 <payten.tyler@gmail.com>
2022-04-18 09:46:59 -0600

    Initial commit

---
+++ gitPractice/gitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeApp.swift
+//  gitPractice
+//
+//  Created by Payten O'Driscoll on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct gitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
